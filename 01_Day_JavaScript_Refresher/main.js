4baddd7b434c209211263f6452d8da72b3144c47
Izzy <isidro.zapata01@utrgv.edu>
2026-01-08 16:06:03 -0600

    Sync master

---
+++ 01_Day_JavaScript_Refresher/main.js
@@ -0,0 +1,8 @@
+let firstName = 'Joe'
+console.log(firstName)
+firstName = 'Job'
+console.log(firstName)
+
+const numArr = [0,3.14,32,9.81,100,1000]
+console.log('Length of number array: ',numArr.length)
+
